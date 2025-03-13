e69610d0856d8206c57e8887c22c3e7acfc2fb39
ParkGana <gnchoco97@gmail.com>
2025-03-13 13:52:11 +0900

    setting : pretendard 폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
+import localFont from 'next/font/local';
 import './globals.css';
+
+const pretendard = localFont({
+  src: '../fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '100 900',
+  variable: '--font-pretendard'
+});
 
 export default function RootLayout({
   children
@@ -7,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className={`${pretendard.variable} font-pretendard`}>{children}</body>
     </html>
   );
 }
